2c44c69932259c97c121e33c60a875f517ae80de
Angzn <gangshuai08@gmail.com>
2014-09-26 14:37:48 +0800

    1、添加UITableView类别

---
+++ SCFramework/SCFramework/SCFramework/Category/UITableView+SCAddition.m
@@ -0,0 +1,13 @@
+//
+//  UITableView+SCAddition.m
+//  SCFramework
+//
+//  Created by Angzn on 9/26/14.
+//  Copyright (c) 2014 Richer VC. All rights reserved.
+//
+
+#import "UITableView+SCAddition.h"
+
+@implementation UITableView (SCAddition)
+
+@end
