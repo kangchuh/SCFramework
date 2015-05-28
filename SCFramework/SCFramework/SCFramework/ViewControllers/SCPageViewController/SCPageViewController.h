f2949711114939f65a7916c55bdc577f79cbb537
Angzn <gangshuai08@gmail.com>
2015-05-28 11:53:59 +0800

    1、新增SCPageViewController

---
+++ SCFramework/SCFramework/SCFramework/ViewControllers/SCPageViewController/SCPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  SCPageViewController.h
+//  SCFramework
+//
+//  Created by Angzn on 15/5/28.
+//  Copyright (c) 2015年 Richer VC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCPageViewController : UIPageViewController
+
+@end
---
+++ SCFramework/SCFramework/SCFramework/ViewControllers/SCPageViewController/SCPageViewController.m
@@ -0,0 +1,31 @@
+//
+//  SCPageViewController.m
+//  SCFramework
+//
+//  Created by Angzn on 15/5/28.
+//  Copyright (c) 2015年 Richer VC. All rights reserved.
+//
+
+#import "SCPageViewController.h"
+
+@interface SCPageViewController ()
+
+@end
+
+@implementation SCPageViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
