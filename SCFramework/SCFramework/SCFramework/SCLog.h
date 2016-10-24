d496383d761a36bb8c1380b5e5a80ceca372d102
Angzn <gangshuai08@gmail.com>
2016-10-24 10:31:48 +0800

    修改LOG

--- SCFramework/SCFramework/SCFramework/SCLog.h
+++ SCFramework/SCFramework/SCFramework/SCLog.h
@@ -11,33 +11,24 @@
 
 //*
 
-//
-// Plugin : XcodeColors
-//
-#define XCODE_COLORS_ESCAPE     @"\033["
-
-#define XCODE_COLORS_RESET_FG   XCODE_COLORS_ESCAPE @"fg;" // Clear any foreground color
-#define XCODE_COLORS_RESET_BG   XCODE_COLORS_ESCAPE @"bg;" // Clear any background color
-#define XCODE_COLORS_RESET      XCODE_COLORS_ESCAPE @";"   // Clear any foreground or background color
-
-#define DEBUG_LOG_COLOR_PREFIX  @"fg0,160,255;"
-#define DEBUG_LOG_COLOR_CONTENT @"fg65,205,69;"
-#define DEBUG_LOG_COLOR_RED     @"fg255,0,0;"
-#define DEBUG_LOG_COLOR_BLUE    @"fg0,0,255;"
-
 #define DEBUG_LOG_PREFIX        @"< %@:[Line %d] %s >\n"
 #define DEBUG_LOG_FILENAME      [[NSString stringWithUTF8String:__FILE__] lastPathComponent]
 
 // Only display output when the DEBUG setting
 #ifdef DEBUG
-#define DLog(frmt, ...)     { NSLog((XCODE_COLORS_ESCAPE DEBUG_LOG_COLOR_PREFIX DEBUG_LOG_PREFIX XCODE_COLORS_RESET \
-                                    XCODE_COLORS_ESCAPE DEBUG_LOG_COLOR_CONTENT frmt XCODE_COLORS_RESET), \
-                                    DEBUG_LOG_FILENAME, __LINE__, __FUNCTION__, ##__VA_ARGS__); }
-#define DRedLog(frmt, ...)  { NSLog((XCODE_COLORS_ESCAPE DEBUG_LOG_COLOR_RED DEBUG_LOG_PREFIX frmt XCODE_COLORS_RESET), \
-                                    DEBUG_LOG_FILENAME, __LINE__, __FUNCTION__, ##__VA_ARGS__); }
-#define DBlueLog(frmt, ...) { NSLog((XCODE_COLORS_ESCAPE DEBUG_LOG_COLOR_BLUE DEBUG_LOG_PREFIX frmt XCODE_COLORS_RESET), \
-                                    DEBUG_LOG_FILENAME, __LINE__, __FUNCTION__, ##__VA_ARGS__); }
-#define ELog(err)           { if (err) DRedLog(@"%@", err); }
+#define DLog(frmt, ...)         { NSLog(DEBUG_LOG_PREFIX frmt, \
+                                        DEBUG_LOG_FILENAME, \
+                                        __LINE__, __FUNCTION__, \
+                                        ##__VA_ARGS__); }
+#define DRedLog(frmt, ...)      { NSLog(DEBUG_LOG_PREFIX frmt, \
+                                        DEBUG_LOG_FILENAME, \
+                                        __LINE__, __FUNCTION__, \
+                                        ##__VA_ARGS__); }
+#define DBlueLog(frmt, ...)     { NSLog(DEBUG_LOG_PREFIX frmt, \
+                                        DEBUG_LOG_FILENAME, \
+                                        __LINE__, __FUNCTION__, \
+                                        ##__VA_ARGS__); }
+#define ELog(err)               { if (err) DRedLog(@"%@", err); }
 #else
 #define DLog(frmt, ...)
 #define DRedLog(frmt, ...)
@@ -47,9 +38,10 @@
 
 // ALog always displays output regardless of the DEBUG setting
 #ifndef ALog
-#define ALog(frmt, ...)     { NSLog((XCODE_COLORS_ESCAPE DEBUG_LOG_COLOR_PREFIX DEBUG_LOG_PREFIX XCODE_COLORS_RESET \
-                                    XCODE_COLORS_ESCAPE DEBUG_LOG_COLOR_CONTENT frmt XCODE_COLORS_RESET), \
-                                    DEBUG_LOG_FILENAME, __LINE__, __FUNCTION__, ##__VA_ARGS__); }
+#define ALog(frmt, ...)         { NSLog(DEBUG_LOG_PREFIX frmt, \
+                                        DEBUG_LOG_FILENAME, \
+                                        __LINE__, __FUNCTION__, \
+                                        ##__VA_ARGS__); }
 #endif
 
 /*/
